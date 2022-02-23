fde37da65ca2aeb47471eccb87a3da7d60f11fc7
CountChangye <121116728@qq.com>
2022-02-23 23:51:51 +0800

    __version__ = "0.0.4"
    Signed-off-by: CountChangye <121116728@qq.com>

--- Strings2/strings.py
+++ Strings2/strings.py
@@ -5,7 +5,7 @@
 #  Vestibulum commodo. Ut rhoncus gravida arcu.
 
 __author__ = "CountChangye <121116728@qq.com>"
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 
 class string(str):
@@ -35,11 +35,25 @@ class string(str):
         return self.character_set[index - 1]
 
     # 判断某个字符在字符串中出现了多少次
-    def judgment_times(self, target_character: str):
-        return sum(
-            single_character == target_character
-            for single_character in self.character_set
-        )
+    # 条件参数output_indexs
+    # 决定是否输出字符在字符串中出现的位置索引
+    # 如果输出，则output_indexs==True;反之，则output_indexs==False
+    # 如果输出索引，则字符出现次数会在索引列表的最后出现
+    def judgment_times(self, target_character: str, output_indexs: bool):
+        if not output_indexs:
+            return sum(
+                single_character == target_character
+                for single_character in self.character_set
+            )
+        indexs = []
+        index = 0
+        for counter, single_character in enumerate(self.character_set):
+            if single_character == target_character:
+                indexs.append(counter)
+                index += 1
+        # 将字符出现次数添加到索引列表的最后
+        indexs.append(index)
+        return indexs
 
 
 if __name__ == "__main__":
@@ -47,4 +61,4 @@ if __name__ == "__main__":
     x = s.add(", world")
     print(type(x))
     print(s.decompose(5))
-    print(s.judgment_times('l'))
+    print(s.judgment_times('l', True))
