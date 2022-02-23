c8a703410273e0dd5934cc4b41036f753677eb25
CountChangye <121116728@qq.com>
2022-02-23 15:33:07 +0800

    version=0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 #  Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
 #  Vestibulum commodo. Ut rhoncus gravida arcu.
 import setuptools
-from Strings import strings
+from Strings2 import strings
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
