c20e15911d1b696f89d63f115071269566418d4e
CountChangye <121116728@qq.com>
2022-02-23 14:26:30 +0800

    初始化

---
+++ setup.py
@@ -0,0 +1,21 @@
+#  Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+#  Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+#  Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+#  Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+#  Vestibulum commodo. Ut rhoncus gravida arcu.
+from distutils.core import setup
+
+setup(
+    name='mydates',
+    version='0.0.1',
+    author='X.X',
+    author_email='xx@example.com',
+    url='www.example.com',
+    license='LICENSE',
+    packages=['mydates'],
+    description='An example of building Python package.',
+    install_requires=[
+        # 'python>=3.6.0',
+        # 'pandas>=0.10.0'
+    ]
+)
---
+++ string.py
@@ -0,0 +1,28 @@
+#  Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+#  Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+#  Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+#  Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+#  Vestibulum commodo. Ut rhoncus gravida arcu.
+
+class string(str):
+    """ Python strings for humans """
+
+    def len(self):
+        return self.__len__()
+
+    @property
+    def length(self):
+        return self.len()
+
+    @property
+    def size(self):
+        return self.length
+
+    def add(self, value):
+        return self + string(value)
+
+
+if __name__ == "__main__":
+    s = string("Hello")
+    x = s.add(", world")
+    print(type(x))
